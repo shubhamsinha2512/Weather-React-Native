579f2403b4885cab1ae8784515731bf068759bad
Shubham Sinha <shubham.sinha2512@gmail.com>
2021-06-19 11:41:23 +0530

    default icons added

---
+++ App.js
@@ -0,0 +1,168 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image
+} from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient'
+
+import icon1 from './src/common/icons/day/113.png'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Feather from 'react-native-vector-icons/Feather'
+import Fontisto from 'react-native-vector-icons/Fontisto'
+
+import {DayIcons, NightIcons} from './src/common/icons'
+
+const App = () => {
+    return (
+
+      <SafeAreaView>
+        <ScrollView style={styles.container}>
+        <LinearGradient
+        start={{x: 0.0, y: 0.25}} 
+        end={{x: 0.5, y: 1.0}}
+        // locations={[0,0.5,0.6]}
+        colors={['#eeb730', '#f19727', '#e9631d']} 
+        style={styles.linearGradient}
+      >
+ 
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon113} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon116} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon119} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon122} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon143} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon176} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon179} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon182} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon185} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon200} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon227} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon230} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon248} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon260} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon263} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon266} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon281} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon284} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon293} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon296} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon299} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon302} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon305} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon308} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon311} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon314} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon317} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon320} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon323} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon326} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon329} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon332} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon335} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon338} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon350} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon353} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon356} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon359} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon362} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon365} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon368} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon371} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon374} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon377} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon386} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon389} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon392} />
+        <Image style={{width:64, height:64}} source={NightIcons.n_icon395} />
+  
+        {/* day icons */}
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon113} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon116} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon119} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon122} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon143} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon176} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon179} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon182} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon185} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon200} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon227} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon230} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon248} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon260} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon263} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon266} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon281} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon284} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon293} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon296} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon299} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon302} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon305} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon308} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon311} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon314} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon317} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon320} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon323} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon326} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon329} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon332} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon335} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon338} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon350} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon353} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon356} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon359} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon362} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon365} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon368} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon371} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon374} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon377} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon386} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon389} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon392} />
+        <Image style={{width:64, height:64}} source={DayIcons.d_icon395} />
+
+        {/* <MaterialIcons name={'wb-sunny'} size={18} color={'#000'} />
+        <MaterialIcons name={'bolt'} size={18} color={'#000'} />
+        <Feather name='sunrise' size={24} color='#000' />
+        <Fontisto name='day-sunny' size={128} color='#fff' /> */}
+      </LinearGradient>
+
+        </ScrollView>
+      </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    display:'flex',
+    height:'90%'
+  },
+  linearGradient:{
+    color:'#fff',
+    height:'100%'
+  },
+  text: {
+    color:'#fff',
+    fontSize:48
+  }
+});
+
+export default App;
---
+++ src/common/icons/index.js
@@ -0,0 +1,201 @@
+//day
+export const d_icon113 = require('./day/113.png')
+export const d_icon116 = require('./day/116.png')
+export const d_icon119 = require('./day/119.png')
+export const d_icon122 = require('./day/122.png')
+export const d_icon143 = require('./day/143.png')
+export const d_icon176 = require('./day/176.png')
+export const d_icon179 = require('./day/179.png')
+export const d_icon182 = require('./day/182.png')
+export const d_icon185 = require('./day/185.png')
+export const d_icon200 = require('./day/200.png')
+export const d_icon227 = require('./day/227.png')
+export const d_icon230 = require('./day/230.png')
+export const d_icon248 = require('./day/248.png')
+export const d_icon260 = require('./day/260.png')
+export const d_icon263 = require('./day/263.png')
+export const d_icon266 = require('./day/266.png')
+export const d_icon281 = require('./day/281.png')
+export const d_icon284 = require('./day/284.png')
+export const d_icon293 = require('./day/293.png')
+export const d_icon296 = require('./day/296.png')
+export const d_icon299 = require('./day/299.png')
+export const d_icon302 = require('./day/302.png')
+export const d_icon305 = require('./day/305.png')
+export const d_icon308 = require('./day/308.png')
+export const d_icon311 = require('./day/311.png')
+export const d_icon314 = require('./day/314.png')
+export const d_icon317 = require('./day/317.png')
+export const d_icon320 = require('./day/320.png')
+export const d_icon323 = require('./day/323.png')
+export const d_icon326 = require('./day/326.png')
+export const d_icon329 = require('./day/329.png')
+export const d_icon332 = require('./day/332.png')
+export const d_icon335 = require('./day/335.png')
+export const d_icon338 = require('./day/338.png')
+export const d_icon350 = require('./day/350.png')
+export const d_icon353 = require('./day/353.png')
+export const d_icon356 = require('./day/356.png')
+export const d_icon359 = require('./day/359.png')
+export const d_icon362 = require('./day/362.png')
+export const d_icon365 = require('./day/365.png')
+export const d_icon368 = require('./day/368.png')
+export const d_icon371 = require('./day/371.png')
+export const d_icon374 = require('./day/374.png')
+export const d_icon377 = require('./day/377.png')
+export const d_icon386 = require('./day/386.png')
+export const d_icon389 = require('./day/389.png')
+export const d_icon392 = require('./day/392.png')
+export const d_icon395 = require('./day/395.png')
+
+//night
+export const n_icon113 = require('./night/113.png')
+export const n_icon116 = require('./night/116.png')
+export const n_icon119 = require('./night/119.png')
+export const n_icon122 = require('./night/122.png')
+export const n_icon143 = require('./night/143.png')
+export const n_icon176 = require('./night/176.png')
+export const n_icon179 = require('./night/179.png')
+export const n_icon182 = require('./night/182.png')
+export const n_icon185 = require('./night/185.png')
+export const n_icon200 = require('./night/200.png')
+export const n_icon227 = require('./night/227.png')
+export const n_icon230 = require('./night/230.png')
+export const n_icon248 = require('./night/248.png')
+export const n_icon260 = require('./night/260.png')
+export const n_icon263 = require('./night/263.png')
+export const n_icon266 = require('./night/266.png')
+export const n_icon281 = require('./night/281.png')
+export const n_icon284 = require('./night/284.png')
+export const n_icon293 = require('./night/293.png')
+export const n_icon296 = require('./night/296.png')
+export const n_icon299 = require('./night/299.png')
+export const n_icon302 = require('./night/302.png')
+export const n_icon305 = require('./night/305.png')
+export const n_icon308 = require('./night/308.png')
+export const n_icon311 = require('./night/311.png')
+export const n_icon314 = require('./night/314.png')
+export const n_icon317 = require('./night/317.png')
+export const n_icon320 = require('./night/320.png')
+export const n_icon323 = require('./night/323.png')
+export const n_icon326 = require('./night/326.png')
+export const n_icon329 = require('./night/329.png')
+export const n_icon332 = require('./night/332.png')
+export const n_icon335 = require('./night/335.png')
+export const n_icon338 = require('./night/338.png')
+export const n_icon350 = require('./night/350.png')
+export const n_icon353 = require('./night/353.png')
+export const n_icon356 = require('./night/356.png')
+export const n_icon359 = require('./night/359.png')
+export const n_icon362 = require('./night/362.png')
+export const n_icon365 = require('./night/365.png')
+export const n_icon368 = require('./night/368.png')
+export const n_icon371 = require('./night/371.png')
+export const n_icon374 = require('./night/374.png')
+export const n_icon377 = require('./night/377.png')
+export const n_icon386 = require('./night/386.png')
+export const n_icon389 = require('./night/389.png')
+export const n_icon392 = require('./night/392.png')
+export const n_icon395 = require('./night/395.png')
+
+export const DayIcons = {
+    d_icon113,
+    d_icon116,
+    d_icon119,
+    d_icon122,
+    d_icon143,
+    d_icon176,
+    d_icon179,
+    d_icon182,
+    d_icon185,
+    d_icon200,
+    d_icon227,
+    d_icon230,
+    d_icon248,
+    d_icon260,
+    d_icon263,
+    d_icon266,
+    d_icon281,
+    d_icon284,
+    d_icon293,
+    d_icon296,
+    d_icon299,
+    d_icon302,
+    d_icon305,
+    d_icon308,
+    d_icon311,
+    d_icon314,
+    d_icon317,
+    d_icon320,
+    d_icon323,
+    d_icon326,
+    d_icon329,
+    d_icon332,
+    d_icon335,
+    d_icon338,
+    d_icon350,
+    d_icon353,
+    d_icon356,
+    d_icon359,
+    d_icon362,
+    d_icon365,
+    d_icon368,
+    d_icon371,
+    d_icon374,
+    d_icon377,
+    d_icon386,
+    d_icon389,
+    d_icon392,
+    d_icon395,
+}
+
+export const NightIcons = {
+    n_icon113,
+    n_icon116,
+    n_icon119,
+    n_icon122,
+    n_icon143,
+    n_icon176,
+    n_icon179,
+    n_icon182,
+    n_icon185,
+    n_icon200,
+    n_icon227,
+    n_icon230,
+    n_icon248,
+    n_icon260,
+    n_icon263,
+    n_icon266,
+    n_icon281,
+    n_icon284,
+    n_icon293,
+    n_icon296,
+    n_icon299,
+    n_icon302,
+    n_icon305,
+    n_icon308,
+    n_icon311,
+    n_icon314,
+    n_icon317,
+    n_icon320,
+    n_icon323,
+    n_icon326,
+    n_icon329,
+    n_icon332,
+    n_icon335,
+    n_icon338,
+    n_icon350,
+    n_icon353,
+    n_icon356,
+    n_icon359,
+    n_icon362,
+    n_icon365,
+    n_icon368,
+    n_icon371,
+    n_icon374,
+    n_icon377,
+    n_icon386,
+    n_icon389,
+    n_icon392,
+    n_icon395,
+}
